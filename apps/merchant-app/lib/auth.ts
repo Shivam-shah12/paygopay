ad37989ed543ea946de5df13fe5144dc5ac4f5f4
Shivam Shah <shivamshah781@gmail.com>
2024-10-28 13:37:10 +0530

    resolve build error

--- apps/merchant-app/lib/auth.ts
+++ apps/merchant-app/lib/auth.ts
@@ -1,48 +1,51 @@
 import GoogleProvider from "next-auth/providers/google";
 import db from "@repo/db/client";
+import { AdapterUser } from "next-auth/adapters"; // Import this type for better type compatibility
+import { Account, Profile, User } from "next-auth";
 
 export const authOptions = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
-        })
-    ],
-    callbacks: {
-      async signIn({ user, account }: {
-        user: {
-          email: string;
-          name: string
-        },
-        account: {
-          provider: "google" | "github"
-        }
-      }) {
-        console.log("hi signin")
-        if (!user || !user.email) {
-          return false;
-        }
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    async signIn(params: {
+      user: User | AdapterUser;
+      account: Account | null;
+      profile?: Profile;
+      email?: { verificationRequest?: boolean };
+      credentials?: Record<string, unknown>;
+    }) {
+      const { user, account } = params;
 
-        await db.merchant.upsert({
-          select: {
-            id: true
-          },
-          where: {
-            email: user.email
-          },
-          create: {
-            email: user.email,
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          },
-          update: {
-            name: user.name,
-            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
-          }
-        });
+      console.log("hi signin");
 
-        return true;
+      if (!user || !user.email) {
+        return false;
       }
+
+      await db.merchant.upsert({
+        select: {
+          id: true,
+        },
+        where: {
+          email: user.email,
+        },
+        create: {
+          email: user.email,
+          name: user.name || "", // Default to empty string if name is undefined
+          auth_type: account?.provider === "google" ? "Google" : "Github", // Handle null for account
+        },
+        update: {
+          name: user.name || "", // Default to empty string if name is undefined
+          auth_type: account?.provider === "google" ? "Google" : "Github", // Handle null for account
+        },
+      });
+
+      return true;
     },
-    secret: process.env.NEXTAUTH_SECRET || "secret"
-  }
+  },
+  secret: process.env.NEXTAUTH_SECRET || "secret",
+};
